9681a80ab327e165162660ea6956d7320a0b20da
aashish <u1174579@utah.edu>
2021-03-01 21:41:41 -0700

    adding leaf install script

---
+++ bin/leaf_install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Install Python 3.6 and Pip3
+apt update
+echo -ne '\n' | add-apt-repository ppa:deadsnakes/ppa
+apt install -y python3.6 python3-pip
+
+# Clone Leaf
+cd /opt && git clone https://github.com/TalwalkarLab/leaf
+pip3 install -r /opt/leaf/requirements.txt
+
+
+
