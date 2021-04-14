18558216b6904f30c6f915186450ab6d4dd3f1ba
agottipati9 <44129794+agottipati9@users.noreply.github.com>
2021-04-14 09:18:52 -0600

    add tensorflow 2 environment

--- bin/install_ibmfl.sh
+++ bin/install_ibmfl.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-echo "Creating conda environment..."
+echo "Creating tf1 conda environment..."
 conda create -n tf1 python=3.6 tensorflow=1.15 -y
 conda activate tf1
 echo "Created tf1 environment. Installing IBM FL..."
@@ -11,4 +11,15 @@ jupyter notebook --generate-config
 echo "c.NotebookApp.allow_origin = '*'" >> ~/.jupyter/jupyter_notebook_config.py
 echo "c.NotebookApp.ip = '0.0.0.0'" >> ~/.jupyter/jupyter_notebook_config.py
 ufw allow 8888
+echo "Installed Jupyter Notebook"
+
+echo "Creating tf2 conda environment..."
+conda create -n tf2 python=3.6 -y
+conda activate tf2
+echo "Created tf2 environment. Installing IBM FL..."
+cd /mydata/federated-learning-lib/federated-learning-lib && pip install ./federated_learning_lib-1.0.4-py3-none-any.whl
+echo "Installed IBM FL. Installing Jupyter Notebook..."
+conda install tensorflow -y
+echo "Installing Jupyter Notebook..."
+conda install -c conda-forge notebook -y
 echo "Installed Jupyter Notebook"
