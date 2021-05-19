72988bea7aea0ee843a2fcb0fe477d99c7b5e77c
agottipati9 <44129794+agottipati9@users.noreply.github.com>
2021-05-18 18:40:59 -0600

    use fork

--- bin/install_ibmfl.sh
+++ bin/install_ibmfl.sh
@@ -3,7 +3,7 @@ echo "Creating tf1 conda environment..."
 conda create -n tf1 python=3.6 tensorflow=1.15 -y
 conda activate tf1
 echo "Created tf1 environment. Installing IBM FL..."
-cd /mydata/ && git clone https://github.com/IBM/federated-learning-lib
+cd /mydata/ && git clone https://github.com/agottipati9/federated-learning-lib
 cd /mydata/federated-learning-lib/federated-learning-lib && pip install ./federated_learning_lib-1.0.4-py3-none-any.whl
 echo "Installed IBM FL. Installing Jupyter Notebook..."
 conda install -c conda-forge notebook -y
